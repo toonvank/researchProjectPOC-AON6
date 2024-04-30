5a0e57b9f2686f66149d7594f7db77853a6ee6fb
Toon <toonvankimmenade@gmail.com>
2024-04-30 16:53:35 +0200

    Project initialization

---
+++ backend/src/main/java/be/pxl/researchproject/poc/PocApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.researchproject.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApplication.class, args);
+	}
+
+}
