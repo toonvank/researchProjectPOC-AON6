9fc727bb0e2a86b4a18a1a8d4e809ccd3450640d
Toon <toonvankimmenade@gmail.com>
2024-05-01 17:05:57 +0200

    Added proxy to allow mixed content #3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {ref} from "vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_BASE_URL,
+        target: ref(import.meta.env.VITE_API_BASE_URL).value,
         changeOrigin: true,
         secure: false // This disables SSL certificate verification
       }
