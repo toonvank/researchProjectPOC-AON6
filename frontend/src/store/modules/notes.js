7f37ee8eb71eb84fbe6a6a303f4f1af53999fbcf
Toon <toonvankimmenade@gmail.com>
2024-05-02 19:05:44 +0200

    Secure backend through cloudflare

--- frontend/src/store/modules/notes.js
+++ frontend/src/store/modules/notes.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 import axios from 'axios';
 import { ref } from 'vue';
 
-const url = "http://84.195.205.124:8442/api/notes/"
+const url = "https://pocapi.toonvank.online/"
 console.log(url);
 
 export const notesStore = defineStore('notes', {
