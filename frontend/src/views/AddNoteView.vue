8b7027c1b45a83269284570ebe5cdbe318011688
Toon <toonvankimmenade@gmail.com>
2024-05-07 23:01:27 +0200

    Url fix #9 Mistake in path

--- frontend/src/views/AddNoteView.vue
+++ frontend/src/views/AddNoteView.vue
@@ -108,7 +108,7 @@ export default {
         showDenyButton: true,
         confirmButtonText: `Upload a photo`,
         denyButtonText: `Use the camera`,
-        imageUrl: "https://dav.toonvank.online/images/" + this.note.photo_url,
+        imageUrl: "https://dav.toonvank.online/" + this.note.photo_url,
       }).then((result) => {
         if (result.isConfirmed) {
           // Upload a photo
